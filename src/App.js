0433247c040bcf14e31becb5f28063c658d5bd10
Osandi26 <osandipabasara@gmail.com>
2025-12-24 00:14:33 +0530

    estate-agent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import properties from "./data/properties.json";
 
 function App() {
+  console.log(properties);
+  
   return (
     <div>
       <h1>Estate Agent App</h1>
